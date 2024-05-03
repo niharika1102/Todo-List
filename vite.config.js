e6a866c33c18c505f60e40a010c6b7084e231f58
Niharika <97402437+niharika1102@users.noreply.github.com>
2024-05-03 09:44:41 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(
+  {
+    plugins: [react()],
+    server: {
+      port: 3001 // Specify your desired port number here
+    }
+  }
+)
+
